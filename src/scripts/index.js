85989ee961177da7e01ddf145a71415b70c5c6c5
marlexxa <marlexxa@gmail.com>
2021-02-23 00:28:59 +0100

    fix(menu): close menu after choosing menu item

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,9 +2,17 @@
 
 const hamburgerButton = document.querySelector('.hamburger');
 const menu = document.querySelector('.menu');
+const menu_items = document.querySelectorAll('.items__item');
 
 hamburgerButton.addEventListener('click', function (event) {
   event.preventDefault();
   this.classList.toggle('hamburger--open');
   menu.classList.toggle('menu--open');
+});
+
+menu_items.forEach((item) => {
+  item.addEventListener('click', function () {
+    hamburgerButton.classList.remove('hamburger--open');
+    menu.classList.remove('menu--open');
+  });
 });
